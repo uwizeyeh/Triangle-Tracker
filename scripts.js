edd3b2c3ea645a23e47c9d003dee5a686c39b38f
uwizeyeh <uwizeyimanahulde1@gmail.com>
2018-12-14 14:46:38 +0200

    Add header in index.html

--- scripts.js
+++ scripts.js
@@ -1,24 +1,15 @@
 function getResult(){
-    var hypotenuse= parseFloat(prompt("side hypotenuse"));
-    var opposite = parseFloat(prompt("side opposite"));
-    var adjacent = parseFloat(prompt("side adjacent"));
-    /* var determine = function(hypotenuse,opposite, adjacent){
-    if (hypotenuse === opposite && opposite === adjacent && hypotenuse === adjacent){
+    var side1= parseFloat(document.getElementById('side1').value);
+    var side2 = parseFloat(document.getElementById('side2').value);
+    var side3 = parseFloat(document.getElementById('side3').value);
+    
+    if(side1 === side2 && side2 === side3){
     alert("Equilateral");
     }
-    else if ((hypotenuse === opposite && opposite === adjacent) (hypotenuse != adjacent && opposite !== adjacent));
+    else if(side1 === side2 || side1 === side3 || side2 === side3){
     alert("Isosceles");
     }
-    determine(); */
-    
-    
-    if(hypotenuse === opposite && hypotenuse === hypotenuse && opposite === adjacent){
-    alert("Equilateral");
-    }
-    else if(hypotenuse === opposite || hypotenuse === adjacent || opposite === adjacent){
-    alert("Isosceles");
-    }
-    else if((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
+    else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
     alert("Not a definite triangle");
     }
     else{
